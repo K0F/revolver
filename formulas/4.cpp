39bdad49afba823f86a79f078f902a4c0755f04d
Kryštof Pešek <krystof.pesek@gmail.com>
2019-03-31 21:43:17 +0200

    working example of pixel access

--- formulas/4.cpp
+++ formulas/4.cpp
@@ -1,11 +1,20 @@
 
 if(frameCount==0)
 {
-R=random()%max;
-G=B=R;
+  R=random()%max;
+  G=B=R;
 }
-int siz = pixels.size();
-int sel = (frameCount-7+siz)%siz;
-R+=(pixels[sel]-R)/10.0;
-G+=(pixels[sel+1]-G)/10.0;
-B+=(pixels[sel+2]-B)/10.0;
+int X = x-(width/2);
+int Y = y-(sin(frameCount/25.0*TWO_PI)*height/2+height/2);
+float d = sqrt(X*X+Y*Y);
+
+if(d < 20){
+  R=G=B=(sin(frameCount/5.0*TWO_PI)+1.0)*max/2.0;
+}else{
+  int id = y*width+x;
+  int siz = pixels.size();
+  int sel = id*3+pow(d*3,1.1)/10;
+  R+=(pixels[sel]-R)/1;
+  G+=(pixels[sel+1]-G)/1;
+  B+=(pixels[sel+2]-B)/1;
+}
